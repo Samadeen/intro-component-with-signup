e5792367a24f37beb024a5762e50105823e649c7
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-05-16 19:34:33 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const form = document.getElementById('form');
+const button = document.getElementById('button');
+const firstName = document.querySelector('.firstName');
+const lastName = document.querySelector('.lastName');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+
+console.log(firstName);
+
+// console.log(firstName, lastName, email, password);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const fName = firstName.value;
+  const lName = lastName.value;
+  const emailVal = email.value;
+  const passwordVal = password.value;
+  console.log(fName, lName, emailVal, passwordVal);
+
+  if (fName === '') {
+    firstName.classList.add('error');
+  } else {
+    firstName.classList.remove('error');
+  }
+
+  if (lName === '') {
+    lastName.classList.add('error');
+  } else {
+    lastName.classList.remove('error');
+  }
+
+  if (!validateEmail(emailVal) || emailVal === '') {
+    email.classList.add('error');
+  } else {
+    email.classList.remove('error');
+  }
+
+  if (passwordVal === '') {
+    password.classList.add('error');
+  } else {
+    password.classList.remove('error');
+  }
+});
+
+//Validate email
+function validateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
